9419d811d5cda6d935c52a6bca398cc0ea324a3a
Tomáš Janoušek <tomaszbrna@gmail.com>
2023-06-10 21:35:13 +0200

    Tutorial Updated

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -8,9 +8,26 @@ public class Tutorial : MonoBehaviour
     [SerializeField] TextMeshProUGUI _textRef;
     [SerializeField] GameObject blockingPanel;
     [SerializeField] GameObject continueButton;
-    List<string> tutorial1 = new List<string> { "Welcome to the tutorial", "You can place your hero by clicking on the hero and\n the wanted place on battlefield", "After placing your heroes on the battlefield\n you can click on one of his actions.", "First and second actions are attacks, third action is heal.", "Good luck!" };
-    List<string> tutorial2 = new List<string> { "Welcome to the second tutorial", "As you can see there are multiple tiles,\nwhere you can place your hero.", "The battlefields are specific for each map\n and it's tiles can be occupied by\n other objects, which can be used as cover.", "Try to place one of your heroes behind the tree.",  "Good luck!"};
-    List<string> tutorial3 = new List<string> { "Welcome to the third tutorial", "Now we will tell you something about upgrades.", "You can receive upgrades by buying them in store,\n getting them as reward or the upgrade can be already\n part of the battlefield.", "You can place your upgrades directly on your hero by\n clicking on the upgrade and after that clicking on the hero.", "You can see upgrades placed on hero by hovering over them.", "Good luck!"};
+    List<string> tutorial1 = new List<string> { 
+        "Welcome to the tutorial",
+        "Goal of the game is to destroy all enemy units\nYour units are on the left, enemies on the right.",
+        "You can place your hero by clicking on the hero\non the bottom and then wanted place on battlefield.", 
+        "After placing your heroes on the battlefield\nyou can click on one of his actions.", 
+        "First and second actions are attacks, third action is heal.",
+        "Good luck!" };
+    List<string> tutorial2 = new List<string> { 
+        "Welcome to the second tutorial", 
+        "As you can see there are multiple tiles,\nwhere there are your units.",
+        "The battlefields are specific for each map\n and it's tiles can be occupied by\n other objects, which can be used as cover.", 
+        "Try to place one of your heroes behind the tree.", 
+        "Good luck!"};
+    List<string> tutorial3 = new List<string> { 
+        "Welcome to the third tutorial", 
+        "Now we will tell you something about upgrades.", 
+        "You can receive upgrades by buying them in store,\n getting them as reward or the upgrade can be already\n part of the battlefield.",
+        "You can place your upgrades directly on your hero by\n clicking on the upgrade and after that clicking on the hero.", 
+        "You can see upgrades placed on hero by hovering over them.", 
+        "Good luck!"};
     List<string> texts = new List<string> { "Tutorial 1", "Tutorial 2", "Tutorial 3" };
     public static Tutorial instance { get; private set; }
     int cnt = 0;
